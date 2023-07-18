664a0999884737a7324e18098dac97d6a30d7217
Stcode-96 <stcode96@gmail.com>
2023-07-18 11:38:10 -0700

    putchar

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,20 +1,19 @@
-#include "main.h"
-
-/**
-*main - function prints text as output
-*
-*Return:return 0
-*/
-int main(void)
-{
-	_putchar('_');
-	_putchar('p');
-        _putchar('u');
-        _putchar('t');
-        _putchar('c');
-        _putchar('h');
-        _putchar('a');
-        _putchar('r');
-        _putchar('\n');
-	return(0);	
-}
+1#include "main.h"
+2/**
+3 *main - function prints text as output
+4 *
+5 *Return:return 0
+6 */
+7 int main(void)
+8 {
+9	_putchar('_');
+10	_putchar('p');
+11      _putchar('u');
+12      _putchar('t');
+13	_putchar('c');
+14	_putchar('h');
+15	_putchar('a');
+16      _putchar('r');
+17	_putchar('\n');
+18	return(0);	
+19}
