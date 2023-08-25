740a650b27f5d7afaf780b3af05dedb73fc096de
Stcode-96 <stcode96@gmail.com>
2023-08-24 19:10:21 -0700

    4-free_list.c

--- 0x12-singly_linked_lists/4-free_list.c
+++ 0x12-singly_linked_lists/4-free_list.c
@@ -1,6 +1,12 @@
 #include <stdlib.h>
 #include "lists.h"
 
+/**
+ * free_list - Frees a linked list and its data.
+ * @head: Pointer to the beginning of the list.
+ *
+ * Description: Dettach memory used by the linked list nodes and their strings.
+ */
 void free_list(list_t *head)
 {
 	list_t *temp;
