299f8b75832ea4b4c8e33f98ed0ef9bb30c63bef
Stcode-96 <stcode96@gmail.com>
2023-08-29 16:18:27 -0700

    list int

--- 0x13-more_singly_linked_lists/1-listint_len.c
+++ 0x13-more_singly_linked_lists/1-listint_len.c
@@ -1,5 +1,11 @@
 #include "lists.h"
 
+/**
+ * listint_len - linked list elements
+ * @h: traverse list
+ *
+ * Return: amount of nodes.
+ */
 size_t listint_len(const listint_t *h)
 {
 	size_t num = 0;
