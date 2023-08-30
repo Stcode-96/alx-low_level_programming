6e67ef16a3ae489fbd8f4db627fa9c2f1e9e46bd
Stcode-96 <stcode96@gmail.com>
2023-08-29 17:16:15 -0700

    add node

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,13 +13,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *temp = *head;
 
 	new = malloc(sizeof(listint_t));
-	if(!new)
+	if (!new)
 		return (NULL);
 
 	new->n = n;
 	new->next = NULL;
 
-	if(*head == NULL)
+	if (*head == NULL)
 	{
 		*head = new;
 		return (new);
